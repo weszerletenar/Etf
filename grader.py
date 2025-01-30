319c923f39dce1d99db1255984079fc53b887292
imadiz <weszerle42@gmail.com>
2025-01-30 12:13:35 +0100

    Inital commit 2

--- grader.py
+++ grader.py
@@ -2,4 +2,8 @@ from bs4 import BeautifulSoup
 with open("index.html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
     rootelem = soup.find("html")
-    print(rootelem['lang'])
+    try:
+        assert rootelem["lang"] == "hu"
+        print("1/1, siker!")
+    except:
+        print("A nyelvnek hu-nak kell lennie! 0/1")
